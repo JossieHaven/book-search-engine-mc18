ce18ea0e5ea4ce750b2c4ad3400d5c757c1f3b24
JossieHaven <jossiehaven@gmail.com>
2025-03-13 12:17:41 -0600

    submitting changes to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,6 @@
+import { dirname } from 'path'; 
+import { fileURLToPath } from 'url'; 
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = dirname(__filename);
+import dotenv from 'dotenv';
 import express from 'express';
-import path from 'node:path';
-import db from './config/connection.js';
-import routes from './routes/index.js';
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
